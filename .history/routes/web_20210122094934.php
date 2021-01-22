9497fedadb3200c8a8dd92386df53bb8f3a9b73f
TheoArnal <theo.arnal@ynov.com>
2021-01-22 18:55:18 +0100

    review page

---
+++ .history/routes/web_20210122094934.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('can:manage-users')->group(function(){
+    Route::resource('/users', 'UsersController', ['except' => ['show', 'create', 'store']]);
+    Route::resource('/dashboard', 'DashboardController');
+    Route::resource('/games', 'GamesController');
+
+});
+
+Route::namespace('Member')->prefix('member')->name('member.')->group(function(){
+    Route::resource('/profile', 'ProfileController');
+    Route
+});
